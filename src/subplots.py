3aec331f717efa4f0611ffd77461d918600ff1d3
hmev <hmev@outlook.com>
2022-07-01 14:18:52 +0800

    On-going.

--- src/subplots.py
+++ src/subplots.py
@@ -31,12 +31,24 @@ class GroupBySubplotImpl:
 
     def __init__(self, 
         group: GroupBy, 
+        title_formatter = None,
         **kwargs):
         self._groupby = group
 
         # Fix a bug for pandas.DataFrame.GroupBy
         if self._groupby.level is not None:
             self._groupby.keys = None
+
+        # Combine groupby names:
+        self._groupby.by = self._groupby.level if self._groupby.level is not None else self._groupby.keys
+
+        # Initiate group index:
+        if not isinstance(self._groupby.by, list):
+            self._groupby.index = pandas.Index(self._groupby.indices.keys(), name = self._groupby.by)
+        else:
+            self._groupby.index = pandas.MultiIndex.from_frame(pandas.DataFrame(self._groupby.indices.keys(), columns = self._groupby.by))
+
+        self._title_formatter = "{LABEL}={VALUE}" if title_formatter is None else title_formatter
 
         self._subplots_kwargs = kwargs
 
@@ -54,11 +66,51 @@ class GroupBySubplotImpl:
             for (key, data), ax in zip(self._groupby, flatten_axes(axes)):
                 getattr(data.plot, plotfunc)(ax = ax, *args, **kwargs)
 
+                ax.set_title(self.get_title(key)) 
+
             # if pandas.__version__ >= "1.5.0":
             #     maybe_adjust_figure(fig, bottom = 0.15, top = 0.9, left = 0.1, right = 0.9, wspace = 0.2)
             return fig
 
         return func
+
+    def create_aligned_subplots(self, plotfunc):
+
+        def func(*args, **kwargs):
+            naxes = self._groupby.index.to_frame().nunique().prod()
+
+            fig, axes = create_subplots(
+                naxes=naxes,
+                **self._subplots_kwargs
+            )
+
+            mapped = self._groupby.index.to_frame()
+            mapped["exists"] = True
+            
+            full = self._groupby.index.to_frame().unique()
+            
+            indexes = self._groupby.index.to_frame().apply(lambda d: d.unique())
+            fullindexes = pandas.MultiIndex.from_product(indexes)
+
+            fullindexes = fullindexes.to_frame().reset_index(drop = True)
+
+            fullindexes["exists"] = False
+            fullindexes = fullindexes.update(mapped)
+
+
+            for (key, data), ax in zip(self._groupby, flatten_axes(axes)):
+                getattr(data.plot, plotfunc)(ax = ax, *args, **kwargs)
+
+                if self._title_formatter != "":
+                    ax.set_title(self.get_title(key)) 
+
+        return func
+
+    def get_title(self, key):
+        if not isinstance(self._groupby.by, list):
+            return self._title_formatter.format(LABEL = self._groupby.by, VALUE = key)
+        else:
+            return ",".join([self._title_formatter.format(LABEL = label, VALUE = value) for label, value in zip(self._groupby.by, key)])
 
 @final
 class GroupBySubplot(PandasObject):
@@ -75,6 +127,7 @@ class GroupBySubplot(PandasObject):
         sharex: bool = False,
         sharey: bool = False,
         layout = None,
+        title_formatter = None,
         **kwargs) -> GroupBySubplotImpl:
         '''interface for subplots
             :params: figsize: Pandas' native option to create subplots. A tuple (width, height) in inches
@@ -82,10 +135,13 @@ class GroupBySubplot(PandasObject):
             :params: sharey: Pandas' native option to create subplots. Whether y-axes will be shared among subplots.
             :params: layout: Pandas' native option to create subplots. The layout of the plot: (rows, columns).
 
+            :param: title_formatter: Title formatter of each pair of groupby labels for subplots. By default use "{LABEL}={VALUE}".
+
             :params: **kwargs: Other options to create subplots.
         '''
         return GroupBySubplotImpl(
             self._groupby,
+            title_formatter = title_formatter,
             figsize = figsize,
             sharex = sharex,
             sharey = sharey,
